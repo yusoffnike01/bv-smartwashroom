4a698a7ac11d370add89bfc7ad084e17e235bd0b
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-02-07 03:48:00 +0800

    missing

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express =require('express')
-const serveStatic=require('serve-static')
+const serverStatic=require('serve-static')
 const path=require('path')
 const app=express()
 app.use('/', serverStatic(path.join(__dirname,'/dist'))
